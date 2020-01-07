cac305aa4bd61947854470caf1e8dc59ead4f793
Holger Dörner <holger.doerner@gmail.com>
2020-01-07 11:41:58 +0100

    fpeek: second draft

--- src/fpeek/fpeek.c
+++ src/fpeek/fpeek.c
@@ -25,7 +25,7 @@
  * For more information, please refer to <https://unlicense.org>
  * -----------------------------------------------------------------------
  * 
- * counter - a simple line- and wordcounter for text-files.
+ * fpeek - peeks the content of files.
  * 
  * Author: Holger Dörner <holger.doerner@gmail.com>
  * 
@@ -36,66 +36,105 @@
 #include "termtools.h"
 #include "fpeek_version.h"
 
-int parseArgs(int, LPWSTR *, LPWSTR *);
+#define PEEK_TOP 0
+#define PEEK_BOTTOM 1
+
+typedef struct SETTINGS {
+    int mode;
+    SIZE_T cLines;
+    LPWSTR pbFileName;
+} SETTINGS;
+
+void parseArgs(SETTINGS *, int, LPWSTR *);
+void printHelp(void);
 
 int wmain(int argc, LPWSTR *argv)
 {
     setUnicodeLocale();
 
-    LPWSTR pbFileName = NULL;
-    int mode = parseArgs(argc, argv, &pbFileName);
-    if (!pbFileName) return 1;
+    SETTINGS settings = {
+        .mode = PEEK_TOP,
+        .cLines = 10,
+        .pbFileName = NULL
+    };
+
+    parseArgs(&settings, argc, argv);
 
     FILE *file;
-    _wfopen_s(&file, pbFileName, L"r");
+    _wfopen_s(&file, settings.pbFileName, L"r");
 
     WCHAR lines[10][1024] = {0};
 
-    int j=0, k=0;
-
-    while (fgetws(lines[j], 1023, file))
+    SIZE_T cbLine = sizeof(WCHAR) * 512 * 2;
+    SIZE_T cvLines = sizeof(LPWSTR) * settings.cLines;
+    LPWSTR *pvLines = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, cvLines);
+    if (!pvLines)
     {
-        if (mode == 1)
+        fwprintf_s(stderr, L"* ERROR: Memory allocation\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < (settings.mode ? settings.cLines : 1); ++i)
+    {
+        pvLines[i] = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, cbLine);
+        if (!pvLines[i])
         {
-            if (j < 9) ++j;
-            else j = 0;
+            fwprintf_s(stderr, L"* ERROR: Memory allocation\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    int vectorCounter = 0, lineCounter = 0;
+
+    while (fgetws(pvLines[vectorCounter], 1023, file))
+    {
+        if (settings.mode == PEEK_BOTTOM)
+        {
+            if (vectorCounter < settings.cLines-1) ++vectorCounter;
+            else vectorCounter = 0;
 
         }
         else
         {
-            wprintf_s(L"%s", lines[j]);
-            if (k == 9) break;
+            wprintf_s(L"%s", pvLines[vectorCounter]);
+            if (lineCounter >= settings.cLines) break;
         }
         
-        k++;
+        ++lineCounter;
 
         if (feof(file)) break;
     }
 
     fclose(file);
 
-    // printf("%d %d\n", k, k%10);
-
-    if (mode == 1)
+    if (settings.mode == PEEK_BOTTOM)
     {
-        for (int i = k%10;;)
+        for (int i = lineCounter % settings.cLines;;)
         {
-            wprintf_s(L"%s", lines[i++]);
+            wprintf_s(L"%s", pvLines[i++]);
 
-            if (i == 10) i = 0;
-            else if (i == (k%10)) break;
+            if (i == settings.cLines) i = 0;
+            else if (i == (lineCounter % settings.cLines)) break;
         }
     }
+
+    for (int i = 0; i < settings.cLines; ++i)
+    {
+        if (pvLines[i]) HeapFree(GetProcessHeap(), 0, pvLines[i]);
+    }
+
+    HeapFree(GetProcessHeap(), 0, pvLines);
 }
 
-int parseArgs(int _argc, LPWSTR *_argv, LPWSTR *_fileName)
+void parseArgs(SETTINGS *_settings, int _argc, LPWSTR *_argv)
 {
     if (_argc == 1)
     {
-        // TODO
+        printHelp();
+        exit(EXIT_SUCCESS);
     }
 
-    int mode = 0;
+    (*_settings).mode = PEEK_TOP;
 
     for (int i = 1; i < _argc; ++i)
     {
@@ -103,32 +142,60 @@ int parseArgs(int _argc, LPWSTR *_argv, LPWSTR *_fileName)
         {
             if (_wcsicmp(_argv[i], L"/?") ==0)
             {
-                // TODO
-            }
-            else if (_wcsicmp(_argv[i], L"/TOP") == 0)
-            {
-                // TODO
+                printHelp();
+                exit(EXIT_SUCCESS);
             }
             else if (_wcsicmp(_argv[i], L"/BOT") == 0)
             {
-                mode = 1;
+                (*_settings).mode = PEEK_BOTTOM;
             }
-            else if (_wcsicmp(_argv[i], L"/L") == 0)
+            else if (towupper(_argv[i][1]) == L'L')
             {
-                // TODO
+                LPWSTR tok, context;
+                wcstok_s(_argv[i], L":", &context);
+                tok = wcstok_s(NULL, L":", &context);
+
+                if (tok)
+                {
+                    (*_settings).cLines = _wcstoi64(tok, (LPWSTR *)NULL, 10);
+                }
+                else
+                {
+                    wprintf_s(L"* Argument /L requires a value.\n");
+                    printHelp();
+                    exit(EXIT_FAILURE);
+                }
+                
             }
             else
             {
-                // TODO
+                fwprintf_s(stderr, L"* ERROR: Unkown argument: %s\n", _argv[i]);
+                exit(EXIT_FAILURE);
             }
             
         }
         else
         {
-            (*_fileName) = _argv[i];
+            (*_settings).pbFileName = _argv[i];
         }
-        
-    }
 
-    return mode;
+        if (!_settings->pbFileName)
+        {
+            wprintf_s(L"* Missing filename.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void printHelp()
+{
+    wprintf_s(L"FPEEK.EXE v%hs\n", FPEEK_VERSION);
+    wprintf_s(L"\n");
+    wprintf_s(L"Usage:\n");
+    wprintf_s(L"\tFPEEK.EXE [/? /BOT /L:n] <filename>\n");
+    wprintf_s(L"\n");
+    wprintf_s(L"Arguments:\n");
+    wprintf_s(L"\t/?            - print help\n");
+    wprintf_s(L"\t/BOT          - peek lines from the bottom\n");
+    wprintf_s(L"\t/L:n          - peek <n> lines (default 10)\n");
 }
